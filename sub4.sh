03b8306d067821e0bd874b0e7e77ce3eafea0982
Aws Yousif <aws.yousif@gmail.com>
2021-12-14 14:51:06 +0300

    Add files via upload

--- sub4.sh
+++ sub4.sh
@@ -35,14 +35,3 @@ sleep 1
 done
 
 
-#trap [COMMANDS] [SIGNALS] 
-
-trap "./trap.sh" SIGINT SIGTERM
-echo "Running .. "
-
-while :			# This is the same as "while true".
-do
-         sleep 10	# This script is not really doing anything.
-         echo " Can't interrupt me"
-         ./code
-done
