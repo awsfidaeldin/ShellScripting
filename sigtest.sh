da9afd89acf261bd300722b4c09761446aa15dde
Aws Yousif <aws.yousif@gmail.com>
2022-01-16 22:02:04 +0300

    Update sigtest.sh

--- sigtest.sh
+++ sigtest.sh
@@ -1,23 +1,14 @@
 process_sig()
 
-{
-    
-echo         # blank line to make the output neater
-    
-date
-    
-cal
-
+{  
+    echo         # blank line to make the output neater   
+    date
+    cal
 }
-
 
 trap process_sig 10
 
-
 while :
-
 do
-   
-sleep 1
-
+    sleep 1
 done
